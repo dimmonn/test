d6e821d125c27d3f1a6283d0fe4026261c65e3c0
dimmon <>
2024-07-29 08:44:47 +0200

    corrections of feature-1

--- src/main/java/com/example/test/TestApplication.java
+++ src/main/java/com/example/test/TestApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestApplication {
-
+// Some corrections about f1
     public static void main(String[] args) {
         System.out.println();
         SpringApplication.run(TestApplication.class, args);
