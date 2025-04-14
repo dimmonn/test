7c9696fdf6cd0152a3ad5a1a14b7fcfc3adecaa2
dpolishchuk <dpolishchuk@vmware.com>
2025-04-14 19:36:22 +0200

    some_test_branch last one

--- src/main/java/com/example/test/TestApplication.java
+++ src/main/java/com/example/test/TestApplication.java
@@ -9,6 +9,7 @@ public class TestApplication {
     public static void main(String[] args) {
         System.out.println();
         System.out.println();
+
         System.out.println();
         System.out.println();
         System.out.println();
