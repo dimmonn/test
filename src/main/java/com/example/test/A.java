d1095d2714dadb1bcb8da09d6ae3b34920eb7e0c
dpolishchuk <dpolishchuk@vmware.com>
2025-05-26 07:16:35 +0200

    3rd

--- src/main/java/com/example/test/A.java
+++ src/main/java/com/example/test/A.java
@@ -5,5 +5,9 @@ public class A {
   public static void main(String[] args) {
     System.out.println();
     System.out.println();
+    System.out.println();
+    System.out.println();
+    System.out.println();
+    System.out.println();
   }
 }
