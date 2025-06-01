1fa0606e8333ea632e0062044824ea16e9fcf747
dpolishchuk <dpolishchuk@vmware.com>
2025-06-01 15:15:48 +0200

    defect 2, fourth  candidate

--- src/main/java/com/example/test/Bug.java
+++ src/main/java/com/example/test/Bug.java
@@ -3,13 +3,16 @@ package com.example.test;
 public class Bug {
 
   public static void main(String[] args) {
-    aa1();
+    aa2();
   }
 
   static void aa() {
     System.out.println();
   }
   static void aa1() {
+    System.out.println();
+  }
+  static void aa2() {
     System.out.println();
   }
 }
