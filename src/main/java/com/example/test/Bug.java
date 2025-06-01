a40914dcfb06f71e2bb762eda97939a239be1aa5
dpolishchuk <dpolishchuk@vmware.com>
2025-06-01 15:15:12 +0200

    defect 2, third candidate

--- src/main/java/com/example/test/Bug.java
+++ src/main/java/com/example/test/Bug.java
@@ -3,11 +3,13 @@ package com.example.test;
 public class Bug {
 
   public static void main(String[] args) {
-    aa();
+    aa1();
   }
 
   static void aa() {
     System.out.println();
   }
-
+  static void aa1() {
+    System.out.println();
+  }
 }
