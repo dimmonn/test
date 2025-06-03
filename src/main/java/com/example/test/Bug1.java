8acaf5b0aa92286c56b35c0df1b0faa8340ec6db
dpolishchuk <dpolishchuk@vmware.com>
2025-06-03 10:39:57 +0200

    bug 1

--- src/main/java/com/example/test/Bug1.java
+++ src/main/java/com/example/test/Bug1.java
@@ -1,8 +1,11 @@
 package com.example.test;
 
+import static com.example.test.Bug.aa1;
+
 public class Bug1 {
 
   public static void main(String[] args) {
-    System.out.println();
+    aa1();
   }
+  void aa2(){}
 }
